9488c53f76a33d7c4a8f1f1b4f9f7148d9a933cf
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-05-11 10:48:27 +0300

    insert values to persons table

--- Table-Relations/solved_problems.sql
+++ Table-Relations/solved_problems.sql
@@ -14,3 +14,7 @@ CREATE TABLE persons (
 
 INSERT INTO passports VALUES(101, 'N34FG21B'), (102, 'K65LO4R7'), (103, 'ZE657QP2');
 
+INSERT INTO persons(first_name, salary, passport_id)
+VALUES('Roberto', '43300.00', 102),
+('Tom', 56100.00, 103), 
+('Yana', 60200.00, 101);
