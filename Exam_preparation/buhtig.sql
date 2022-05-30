878c88d2a70b90e64d7f25a32d79b9b450ef24d1
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-05-30 15:04:52 +0300

    solved

--- Exam_preparation/buhtig.sql
+++ Exam_preparation/buhtig.sql
@@ -59,3 +59,76 @@ CREATE TABLE `files` (
     CONSTRAINT `commit_id`
     FOREIGN KEY (commit_id) REFERENCES commits(id)
 );
+
+INSERT INTO issues (`title`, `issue_status`, `repository_id`, `assignee_id`)
+SELECT 
+        CONCAT('Critical Problem With ', f.name, '!') as `title`,
+        'open' AS `issue_status`,
+        CEIL((f.id * 2) / 3) AS `repository_id`,
+        (SELECT contributor_id FROM commits
+            WHERE commits.id = f.commit_id) AS `assignee_id`
+FROM files AS f
+WHERE f.id BETWEEN 46 AND 50;
+
+
+
+UPDATE repositories_contributors AS rc
+SET rc.repository_id = 
+(SELECT rep.id FROM repositories AS rep
+WHERE rep.id NOT IN (SELECT repository_id FROM 
+                    (SELECT repository_id FROM repositories_contributors) AS a)
+                    ORDER BY rep.id LIMIT 1)
+WHERE rc.repository_id = rc.contributor_id;
+
+
+DELETE FROM repositories r
+WHERE ( 
+    SELECT COUNT(*) FROM issues i
+    WHERE i.repository_id = r.id) = 0;
+
+
+DELIMITER $$
+CREATE PROCEDURE udp_commit(
+    username VARCHAR(30),
+    password VARCHAR(10),
+    message VARCHAR(255),
+    issue_id INT
+)
+BEGIN
+    START TRANSACTION;
+    IF ((SELECT COUNT(*) FROM users u WHERE u.username = username) = 0)
+        THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No such user!';
+        ROLLBACK;
+    ELSEIF ((SELECT COUNT(*) FROM users u WHERE u.username = username AND u.password = password) = 0)
+        THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Password is incorrect!';
+        ROLLBACK;
+    ELSEIF ((SELECT COUNT(*) FROM issues i WHERE i.id = issue_id) = 0)
+        THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The issue does not exist!';
+        ROLLBACK;
+    ELSE
+        INSERT INTO commits (`message`, `issue_id`, `repository_id`, `contributor_id`) 
+        VALUES(
+                message, 
+                issue_id, 
+                (SELECT repository_id FROM issues i WHERE i.id = issue_id),
+                (SELECT id FROM users u WHERE u.username = username)
+        );
+
+        UPDATE issues i SET i.issue_status = 'closed'
+        WHERE i.id = issue_id;
+        COMMIT;
+    END IF;
+END $$
+
+CALL udp_commit('WhoDenoteBel', 'ajmISQi*', 'Fixed issue: Invalid', 2);
+
+
+DELIMITER $$
+
+CREATE PROCEDURE udp_findbyextension(extension VARCHAR(50))
+BEGIN
+    SELECT f.id, f.name AS `caption`, CONCAT(f.size, 'KB') AS `user` FROM files f
+    WHERE f.name LIKE CONCAT('%', extension);
+END $$
+
+CALL udp_findbyextension('html');
