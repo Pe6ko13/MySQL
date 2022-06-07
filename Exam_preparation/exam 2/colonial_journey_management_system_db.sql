5e86d5e2b7107d7fc9a228d7ab5dc22b235fbb1e
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-06-07 11:42:00 +0300

    problem 2 solved

--- Exam_preparation/exam 2/colonial_journey_management_system_db.sql
+++ Exam_preparation/exam 2/colonial_journey_management_system_db.sql
@@ -52,3 +52,33 @@ CREATE TABLE travel_cards (
     CONSTRAINT fk_tc_journeys
     FOREIGN KEY (journey_id) REFERENCES journeys(id)
 );
+
+
+INSERT INTO travel_cards (card_number, job_during_journey, colonist_id, journey_id)
+SELECT (
+    CASE WHEN birth_date > '1980-01-01'
+        THEN CONCAT_WS('', YEAR(birth_date), DAY(birth_date), LEFT(ucn, 4))
+        ELSE CONCAT_WS('', YEAR(birth_date), MONTH(birth_date), RIGHT(ucn, 4))
+    END) AS card_number,
+    (
+    CASE WHEN id % 2 = 0 THEN 'Pilot'
+        WHEN id % 3 = 0 THEN 'Cook'
+        ELSE 'Engineer'
+    END) AS job_during_journey,
+    id AS colonist_id,
+    LEFT(ucn, 1) AS journey_id
+FROM colonists WHERE id BETWEEN 96 AND 100;
+
+
+UPDATE journeys
+SET purpose = (
+    CASE WHEN id % 2 = 0 THEN 'Medical'
+        WHEN id % 3 = 0 THEN 'Technical'
+        WHEN id % 5 = 0 THEN 'Educational'
+        WHEN id % 7 = 0 THEN 'Military'
+        ELSE purpose
+    END);
+
+
+DELETE FROM colonists c
+WHERE c.id NOT IN (SELECT colonist_id from travel_cards);
